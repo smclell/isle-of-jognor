982156fac69d1f9f8550fae7855c6e4eb38b73ce
[Sean McLellan] <seanmcl.at.home@gmail.com>
2022-06-09 17:53:19 -0400

    textured land, imported player and villager/guard

---
+++ isle of jognor/Assets/Testing Player stuff/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    /* 
+    You will need to set the player object in Unity. 
+    The object that has this script attached to it, ie the 'Camera Target' object
+    will then align itself with whatever object is set as the 'Player'
+    */
+    void Update()
+    {
+
+        // Align to player position
+        transform.position = player.transform.position;
+
+        // Rotate left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 100, Space.World);
+        }
+
+        // Rotate right
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 100, Space.World);
+        }
+    }
+}
