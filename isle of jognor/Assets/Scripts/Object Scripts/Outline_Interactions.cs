1de2c44f8efcc6be8e07dfad9203a071b3113a82
[Sean McLellan] <seanmcl.at.home@gmail.com>
2022-06-14 13:32:11 -0400

    outline corrections

--- isle of jognor/Assets/Scripts/Object Scripts/Outline_Interactions.cs
+++ isle of jognor/Assets/Scripts/Object Scripts/Outline_Interactions.cs
@@ -4,40 +4,23 @@ using UnityEngine;
 
 public class Outline_Interactions : MonoBehaviour
 {
-    private Renderer render;
-
     private Outline outline;
 
-    [SerializeField, Range(0f, 5f)]
-    private float waitTime = 0.5f;
+    
+    //[SerializeField, Range(0f, 20f)]
+    //private float maxDistance = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         outline = gameObject.GetComponent<Outline>();
         outline.enabled = false;
-        render = gameObject.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (render.isVisible)
-        {
-            StartCoroutine(Outline());
-        }
-        else
-        {
-            outline.enabled = false;
-        }
+        outline.enabled = false;
     }
-
-    IEnumerator Outline()
-    {
-        yield return new WaitForSeconds(waitTime);
-
-        outline.enabled = true;
-    }
-
 
 }
--- isle of jognor/Assets/Scripts/Player Scripts/Interactions.cs
+++ isle of jognor/Assets/Scripts/Player Scripts/Interactions.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
+    private Outline outline;
+
+
+    [SerializeField, Range(0f, 20f)]
+    private float maxDistance = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        //outline.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        // Creates a Ray from the center of the viewport
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if (Physics.Raycast(ray, out hit, maxDistance) && hit.collider.gameObject.CompareTag("Interactable Clue"))
+        {
+            outline = hit.collider.gameObject.GetComponent<Outline>();
+            outline.enabled = true;
+        }
+        else
+        {
+            outline = hit.collider.gameObject.GetComponent<Outline>();
+            outline.enabled = false;
+        }
     }
 }
